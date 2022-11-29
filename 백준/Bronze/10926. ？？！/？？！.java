8f927f5fb57e1f51b16ecb8384afffa5ff40cb9b
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2022-11-29 17:39:42 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	
+		public static void main(String[] args) {
+			Scanner scan = new Scanner(System.in);
+			String id = scan.next();
+			if(id.equals(id)){
+				System.out.println(id+"??!");
+			}
+		}
+
+}
