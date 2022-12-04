92bbb4e022f8e6b8062d1e467f283ac1085ea99a
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2022-12-04 18:32:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+	
+public class Main {
+	
+		public static void main(String[] args)  {
+			
+		Scanner scan = new Scanner(System.in);
+		String input =scan.next();
+		int A =input.charAt(0);
+		System.out.printf("%d", A);
+
+		}
+	}
