50b115b03ff2db17e01836ccc29542399302497a
유선옥 <107903342+YUSUNOK@users.noreply.github.com>
2022-12-05 18:04:42 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int mot=n/7;
+        if(n% 7 !=0){
+            mot ++;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        int answer = mot;
+        return answer;
+    }
+}
